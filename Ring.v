970aa2830febcc8e0aff0dc71e4f402f527c476b
kb <koba@mac-virtualpc.none>
2016-08-22 00:29:22 +0900

    Add section s1_properties

--- Ring.v
+++ Ring.v
@@ -7,36 +7,59 @@ Definition isAbelianGroup(M: Type)
   (forall x y z, d x (d y z) = d z (d y x)) *
   (forall x y z, d (d x z) (d y z) = d x y).
 
-Definition s1_inv: S1 -> S1.
-apply S1_rec with (b := base).
-apply loop^.
-Defined.
+Section s1_properties.
+
+  Definition s1_inv: S1 -> S1.
+  apply S1_rec with (b := base).
+  apply loop^.
+  Defined.
 
 
-Goal (ap s1_inv loop = loop^).
-apply S1_rec_beta_loop.
-Qed.
+  Goal (ap s1_inv loop = loop^).
+  apply S1_rec_beta_loop.
+  Qed.
 
-Goal forall b, s1_inv (s1_inv b) = b.
-refine (S1_ind (fun u => s1_inv (s1_inv u) = u) idpath _).
-rewrite transport_paths_FlFr.
-rewrite ap_idmap.
-unfold s1_inv.
-rewrite ap_compose.
-rewrite S1_rec_beta_loop.
-rewrite ap_V.
-rewrite S1_rec_beta_loop.
-rewrite concat_p1.
-rewrite inv_V.
-apply concat_Vp.
-Qed.
+  Goal forall b, s1_inv (s1_inv b) = b.
+  refine (S1_ind (fun u => s1_inv (s1_inv u) = u) idpath _).
+  rewrite transport_paths_FlFr.
+  rewrite ap_idmap.
+  unfold s1_inv.
+  rewrite ap_compose.
+  rewrite S1_rec_beta_loop.
+  rewrite ap_V.
+  rewrite S1_rec_beta_loop.
+  rewrite concat_p1.
+  rewrite inv_V.
+  apply concat_Vp.
+  Qed.
 
-Definition s1_roll: S1 -> S1 
-  := S1_rec S1 base loop.
+  Context `{Univalence}.
+  Definition s1_sub (a b: base = base): base = base
+    := a @ b^.
+  Theorem loopS1_comm: forall a b: base = base,
+    a @ b = b @ a.
+  intros a b.
+  Admitted.
 
-Definition s1_m: (S1 * S1) -> S1.
-intro x.
+  Definition s1_abelian: isAbelianGroup (base = base) idpath s1_sub.
+  do 3 (try split).
+  apply concat_pV.
+  apply concat_p1.
+  intros x y z.
+  unfold s1_sub.
+  do 2 rewrite inv_pV.
+  rewrite (loopS1_comm z).
+  rewrite concat_p_pp.
+  apply loopS1_comm.
 
-SearchAbout (_ * _).
-apply prod_rec.
+  intros x y z.
+  unfold s1_sub.
+  rewrite inv_pV.
+  rewrite concat_p_pp.
+  rewrite (concat_pp_p _ z^).
+  rewrite concat_Vp.
+  rewrite concat_p1.
+  reflexivity.
+  Defined.
+End s1_properties.
 
